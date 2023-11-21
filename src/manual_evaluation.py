3f8f1603cbce6f0e6c88a0bd729a7ff63f2601bb
Apostolos Dimoulakis <apo.dimu@gmail.com>
2023-11-21 23:56:21 +0200

    v0.5.9

--- src/manual_evaluation.py
+++ src/manual_evaluation.py
@@ -18,7 +18,7 @@ class manual_evaluation_ui:
         self.window = tk.Tk()
 
         # Set window dimensions (width x height)
-        self.window_width = 1000
+        self.window_width = 1410
         self.window_height = 700
 
         # Get the screen width and height
@@ -32,7 +32,7 @@ class manual_evaluation_ui:
         # Set the window size and position
         self.window.geometry(f"{self.window_width}x{self.window_height}+{self.x_position}+{self.y_position}")
 
-    def build(self, img, res, img_fp, mask_fp, n_smoke_pixels, contains_smoke):
+    def build(self, img, mask, combined, res, img_fp, mask_fp, n_smoke_pixels, contains_smoke):
 
         def top_text():
 
@@ -49,7 +49,7 @@ class manual_evaluation_ui:
         def left_text():
 
             top_margin = left_margin = right_margin = bottom_margin = 10
-            text_frame_width = self.window_width - img_outer_frame_shape[0] - 3 * right_margin - left_margin
+            text_frame_width = self.window_width - combined_outer_frame_shape[0] - 3 * right_margin - left_margin
             text_frame_height = self.window_width - 4 * 90 - 40
 
             text_frame = tk.Frame(self.window, width = text_frame_width, height = text_frame_height, bg = 'white')
@@ -61,7 +61,7 @@ class manual_evaluation_ui:
         def upper_button():
 
             top_margin = left_margin = right_margin = bottom_margin = 10
-            button_frame_width = self.window_width - img_outer_frame_shape[0] - 2 * right_margin - left_margin
+            button_frame_width = 170
             button_frame_height = 100
 
             button_frame = tk.Frame(self.window, width = button_frame_width, height = button_frame_height)
@@ -73,7 +73,7 @@ class manual_evaluation_ui:
         def bottom_button():
 
             top_margin = left_margin = right_margin = bottom_margin = 10
-            button_frame_width = self.window_width - img_outer_frame_shape[0] - 2 * right_margin - left_margin
+            button_frame_width = 170
             button_frame_height = 100
 
             button_frame = tk.Frame(self.window, width = button_frame_width, height = button_frame_height)
@@ -103,7 +103,47 @@ class manual_evaluation_ui:
 
         DefaultFont = font.Font(family = 'Lato', size = '15', weight = 'bold')
 
-        ## ! Build image root frame: Begin
+        ## ! Build right image frame: Begin
+
+        ## In this method, all shapes are considered as (width, height)
+        combined_shape = [combined.shape[1], combined.shape[0]]
+
+        right_margin = 10
+        bottom_margin = 10
+
+        combined_outer_frame_shape = (800, 600)
+
+        outer_combined_frame = tk.Frame(self.window, width = combined_outer_frame_shape[0], height = combined_outer_frame_shape[1], bg = 'black')
+        outer_combined_frame.place(x = self.window_width - combined_outer_frame_shape[0] - right_margin, y = self.window_height - combined_outer_frame_shape[1] - bottom_margin)
+
+        ## Resize image while keeping aspect ratio
+        combined_aspect_ratio = combined_shape[1] / combined_shape[0]
+
+        ## Adjust image and inner frame resolution, to fit exactly inside the outer frame
+        combined_shape[0] = combined_outer_frame_shape[0]
+        combined_shape[1] = int(combined_aspect_ratio * combined_shape[0])
+        if combined_shape[1] > combined_outer_frame_shape[1]:
+            combined_shape[1] = combined_outer_frame_shape[1]
+            combined_shape[0] = int(combined_shape[1] / combined_aspect_ratio)
+        combined = cv2.resize(combined, (combined_shape[0], combined_shape[1]))
+        combined = Image.fromarray(combined)
+        combined_tk = ImageTk.PhotoImage(combined)
+        combined_inner_frame_shape = \
+        (
+            combined_shape[0],
+            combined_shape[1]
+        )
+
+        ## Creating a child frame inside the outer image frame. Placing coordinate system places (0, 0) at the upper left edge of the outer frame.
+        inner_combined_frame = tk.Frame(outer_combined_frame, width = combined_inner_frame_shape[0], height = combined_inner_frame_shape[1], bg="white")
+        inner_combined_frame.place(x = combined_outer_frame_shape[0] // 2 - combined_inner_frame_shape[0] // 2, y = combined_outer_frame_shape[1] // 2 - combined_inner_frame_shape[1] // 2)
+
+        combined_label = tk.Label(inner_combined_frame, image = combined_tk, bg = 'white')
+        combined_label.grid(column = 0, row = 0)
+
+        ## ! Build right image frame: End
+
+        ## ! Build left top image frame: Begin
 
         ## In this method, all shapes are considered as (width, height)
         img_shape = [img.shape[1], img.shape[0]]
@@ -111,10 +151,10 @@ class manual_evaluation_ui:
         right_margin = 10
         bottom_margin = 10
 
-        img_outer_frame_shape = (800, 600)
+        img_outer_frame_shape = (400, 300)
 
         outer_img_frame = tk.Frame(self.window, width = img_outer_frame_shape[0], height = img_outer_frame_shape[1], bg = 'black')
-        outer_img_frame.place(x = self.window_width - img_outer_frame_shape[0] - right_margin, y = self.window_height - img_outer_frame_shape[1] - bottom_margin)
+        outer_img_frame.place(x = self.window_width - img_outer_frame_shape[0] - 2 * right_margin - combined_outer_frame_shape[0], y = self.window_height - 2* img_outer_frame_shape[1] - bottom_margin)
 
         ## Resize image while keeping aspect ratio
         img_aspect_ratio = img_shape[1] / img_shape[0]
@@ -141,7 +181,47 @@ class manual_evaluation_ui:
         img_label = tk.Label(inner_img_frame, image = img_tk, bg = 'white')
         img_label.grid(column = 0, row = 0)
 
-        ## ! Build image root frame: End
+        ## ! Build left top image frame: End
+
+        ## ! Build left bottom image frame: Begin
+
+        ## In this method, all shapes are considered as (width, height)
+        mask_shape = [mask.shape[1], mask.shape[0]]
+
+        right_margin = 10
+        bottom_margin = 10
+
+        mask_outer_frame_shape = (400, 300)
+
+        outer_mask_frame = tk.Frame(self.window, width = mask_outer_frame_shape[0], height = mask_outer_frame_shape[1], bg = 'black')
+        outer_mask_frame.place(x = self.window_width - mask_outer_frame_shape[0] - 2 * right_margin - combined_outer_frame_shape[0], y = self.window_height - mask_outer_frame_shape[1] - bottom_margin)
+
+        ## Resize image while keeping aspect ratio
+        mask_aspect_ratio = mask_shape[1] / mask_shape[0]
+
+        ## Adjust image and inner frame resolution, to fit exactly inside the outer frame
+        mask_shape[0] = mask_outer_frame_shape[0]
+        mask_shape[1] = int(mask_aspect_ratio * mask_shape[0])
+        if mask_shape[1] > mask_outer_frame_shape[1]:
+            mask_shape[1] = mask_outer_frame_shape[1]
+            mask_shape[0] = int(mask_shape[1] / mask_aspect_ratio)
+        mask = cv2.resize(mask, (mask_shape[0], mask_shape[1]))
+        mask = Image.fromarray(255 * mask)
+        mask_tk = ImageTk.PhotoImage(mask)
+        mask_inner_frame_shape = \
+        (
+            mask_shape[0],
+            mask_shape[1]
+        )
+
+        ## Creating a child frame inside the outer image frame. Placing coordinate system places (0, 0) at the upper left edge of the outer frame.
+        inner_mask_frame = tk.Frame(outer_mask_frame, width = mask_inner_frame_shape[0], height = mask_inner_frame_shape[1], bg="white")
+        inner_mask_frame.place(x = mask_outer_frame_shape[0] // 2 - mask_inner_frame_shape[0] // 2, y = mask_outer_frame_shape[1] // 2 - mask_inner_frame_shape[1] // 2)
+
+        mask_label = tk.Label(inner_mask_frame, image = mask_tk, bg = 'white')
+        mask_label.grid(column = 0, row = 0)
+
+        ## ! Build left bottom image frame: End
 
         ## Top text
         top_text()
@@ -216,9 +296,9 @@ def manual_evaluation_sequence(BLACKLIST_FP = '../blacklisted_instances.list', p
             else:
                 continue
 
-        combined_img = visuals.combine_img_mask(img = img, mask = mask)
+        combined = visuals.combine_img_mask(img = img, mask = mask)
 
-        ui.build(img = combined_img, res = res, img_fp = img_fp, mask_fp = mask_fp, n_smoke_pixels = n_smoke_pixels, contains_smoke = contains_smoke)
+        ui.build(img = img, mask = mask, combined = combined, res = res, img_fp = img_fp, mask_fp = mask_fp, n_smoke_pixels = n_smoke_pixels, contains_smoke = contains_smoke)
 
         checkpoint(mask_fp = mask_fp)
 
